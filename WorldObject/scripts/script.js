5f311b8c7efce4c5c7234f2bcd93136766fd825b
Rodrigo Antunes <rodrigoantunes@LISALLBYDMX04.local>
2020-02-19 16:32:54 +0000

    Minecraft Spark

---
+++ WorldObject/scripts/script.js
@@ -0,0 +1,47 @@
+/**
+ * (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ */
+
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+
+// How to load in modules
+const Scene = require('Scene');
+const Camera = require('Camera');
+const plane = Scene.root.find('vilinha');
+const heart = Scene.root.find('canvas0');
+// Use export keyword to make a symbol available in scripting debug console
+export const Diagnostics = require('Diagnostics');
+
+// To use variables and functions across files, use export/import keyword
+// export const animationDuration = 10;
+
+// Use import keyword to import a symbol from another file
+// import { animationDuration } from './script.js'
+
+// To access scene objects
+// const directionalLight = Scene.root.find('directionalLight0');
+
+// To access class properties
+// const directionalLightIntensity = directionalLight.intensity;
+
+// To log messages to the console
+// Diagnostics.log('Console message logged from the script.');
+const TouchGestures = require('TouchGestures');
+const planeTransform = plane.transform;
+
+const Animation = require('Animation');
+
+heart.visibility(false);
+// Subscribe to tap gestures
+TouchGestures.onTap().subscribe(function (gesture) {
+    // Log a message to the console when a tap is detected
+    Diagnostics.log('tap gesture detected');
+
+});
